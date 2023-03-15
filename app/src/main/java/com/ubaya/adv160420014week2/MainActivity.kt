e817eaf788b74035251b0896e2d97b1f754a5ee8
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-03-15 15:20:49 +0700

    Adding bottom nav and additional fragments

--- app/src/main/java/com/ubaya/adv160420014week2/MainActivity.kt
+++ app/src/main/java/com/ubaya/adv160420014week2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
         navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
         NavigationUI.setupActionBarWithNavController(this,navController)
+
+        bottomNav.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
